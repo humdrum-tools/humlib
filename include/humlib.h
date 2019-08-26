1f1e6a1281b40a6d6c9fed6666d96221cd619dc0
shanahdt <shanahan.37@MUS-NC215271.local>
2019-08-26 16:54:32 -0400

    added some docs

--- include/humlib.h
+++ include/humlib.h
@@ -1,7 +1,7 @@
 //
 // Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
 // Creation Date: Sat Aug  8 12:24:49 PDT 2015
-// Last Modified: Wed Aug 21 13:34:26 EDT 2019
+// Last Modified: Mon Aug 26 15:38:57 EDT 2019
 // Filename:      humlib.h
 // URL:           https://github.com/craigsapp/humlib/blob/master/include/humlib.h
 // Syntax:        C++11
